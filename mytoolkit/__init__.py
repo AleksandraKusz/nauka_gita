07ec5d45ad459a1be07443ead3a7b73a9eef1250
student <student@ubuntu-42.edu-wsb.local>
2022-05-29 10:38:17 +0200

    Added file with moduls

---
+++ mytoolkit/__init__.py
@@ -0,0 +1,4 @@
+# plik o tej nazwie wgamy do folderu mytoolkit informuje sredowisko ze caly katalog jest
+# PAKIETEM
+# zmienna all zawiera liste modulow zawartych w pakiecie
+__all__ = ["matematyczny", "pomocniczy"]
---
+++ mytoolkit/pomocniczy.py
@@ -0,0 +1,4 @@
+# stworzymy metode bezparametrowa, ktora nie bedzie zwracala wartosci
+def czesc():
+    # blok nalezacy do funkcji
+    print('wywolales mnie z modulu pomocniczy')
